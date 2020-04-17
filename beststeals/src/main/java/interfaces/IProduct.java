2c866079eee5be846e51f44ad6f391805821f633
Jerzy9 <zbylutkonrad@gmail.com>
2020-04-17 13:43:38 +0200

    big update

--- beststeals/src/main/java/interfaces/IProduct.java
+++ beststeals/src/main/java/interfaces/IProduct.java
@@ -1,17 +1,15 @@
 package interfaces;
 
-import java.io.IOException;
-
-public interface IScraper {
+public interface IProduct {
 	
-	public void scrap() throws IOException;
+	public boolean scrap();
 	public String getSiteName(); 
 	public String getSiteLink();
 	public String getName();
 	public String getOldPrice();
 	public String getNewPrice();
-	public String getRemaining();
-	public String getSold();
+	public String getRemainingQuantity();
+	public String getLimitQuantity();
 	public String getImage();
 	
 }
--- beststeals/src/main/java/products/old/Xkom.java
+++ beststeals/src/main/java/products/old/Xkom.java
@@ -1,4 +1,4 @@
-package scrapers;
+package products.old;
 
 import java.io.IOException;
 
@@ -6,26 +6,32 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import interfaces.IProduct;
 import interfaces.IScraper;
+import interfaces.IScrapers;
+import scrapers.Scraper;
 
-public class Xkom implements IScraper {
+public class Xkom implements IScrapers {
 	
 	private String siteLink;
 	private String siteName;
-	public String name;
+	private String name;
 	private String oldPrice, newPrice;
-	private String remaining, sold;
+	private String remaining = "0", sold;
 	private String img;
+	
+	private Element el;
+	private Scraper scraper;
 	
 
 	public Xkom() {
+		scraper = new Scraper();
 		try {
 			scrap();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 	
 	public void scrap() throws IOException {
@@ -35,27 +41,16 @@ public class Xkom implements IScraper {
 		Document doc = Jsoup.connect(siteLink).get();	
 		Element hotShot = doc.getElementById("hotShot");
 		
-		this.name = getStringByClass(hotShot, "product-name", 0);
-		this.oldPrice = getStringByClass(hotShot, "old-price", 0);
-		this.newPrice = getStringByClass(hotShot, "new-price", 0);
-		this.remaining = getStringByClass(hotShot, "gs-quantity", 0);
-		this.sold = getStringByClass(hotShot, "gs-quantity", 0);
+		this.name = scraper.getStringByClass(hotShot, "product-name", 0);
+		this.oldPrice = scraper.getStringByClass(hotShot, "old-price", 0);
+		this.newPrice = scraper.getStringByClass(hotShot, "new-price", 0);
+		this.remaining = scraper.getStringByClass(hotShot, "gs-quantity", 0);
+		this.sold = scraper.getStringByClass(hotShot, "gs-quantity", 0);
 		
 		Element image = hotShot.getElementsByClass("img-responsive").first();
     	this.img = image.absUrl("src");
 		
 		
-	}
-	
-	private String getStringByClass(Element el, String className, int index) {
-		String str;
-		try {
-			str = el.getElementsByClass(className).get(index).text();
-		} catch (Exception e) {
-			System.out.println("NIE ZNALAZLO WARTOSCI w klasie: " + className);
-			str = "null";
-		}
-		return str;
 	}
 
 	public String getSiteName() {
