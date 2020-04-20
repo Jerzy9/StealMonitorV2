a55753f68b682e5645c8f2d7d8aa61d3bbd2661f
Jerzy9 <zbylutkonrad@gmail.com>
2020-04-21 01:20:43 +0200

    pass

--- beststeals/src/main/java/test/LoadetTest.java
+++ beststeals/src/main/java/test/LoadetTest.java
@@ -107,7 +107,7 @@ public class LoadetTest {
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/steal_monitor?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
 		dataSource.setUsername("root");
-		dataSource.setPassword("kapiszony123");
+		dataSource.setPassword("TUTAJ WPISZ SWOJE HAS£O");
 		
 		ProductDAO dao = new ProductDAO(dataSource);
 		Element el = null;
